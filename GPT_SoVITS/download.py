a0383fb82aa2a929ba9b2701e8611ffa28e3022c
Ubuntu <ubuntu@ip-172-31-88-222.ec2.internal>
2025-01-14 03:51:50 +0000

    change to absolute path

--- GPT_SoVITS/download.py
+++ GPT_SoVITS/download.py
@@ -2,4 +2,4 @@ import os, sys
 now_dir = os.getcwd()
 sys.path.insert(0, now_dir)
 from text.g2pw import G2PWPinyin
-g2pw = G2PWPinyin(model_dir="GPT_SoVITS/text/G2PWModel",model_source="GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large",v_to_u=False, neutral_tone_with_five=True)
+g2pw = G2PWPinyin(model_dir="/home/ubuntu/GPT-SoVITS/GPT_SoVITS/text/G2PWModel",model_source="/home/ubuntu/GPT-SoVITS/GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large",v_to_u=False, neutral_tone_with_five=True)
